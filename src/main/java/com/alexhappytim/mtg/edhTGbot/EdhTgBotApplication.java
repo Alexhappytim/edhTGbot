12cf2675ae8efb9b9a0087d3d810e20242f2ca87
Alexhappytim <timoshkovalex@gmail.com>
2025-11-12 01:07:59 +0300

    Init commit

---
+++ src/main/java/com/alexhappytim/mtg/edhTGbot/EdhTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.alexhappytim.mtg.edhTGbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdhTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdhTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexhappytim/mtg/edhTGbot/EdhTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexhappytim.mtg.edhTGbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdhTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
